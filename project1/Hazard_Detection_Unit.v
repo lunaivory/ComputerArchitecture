217fddd80646188bc3677fa47339359c1b95a264
lunaivory <catlily234@gmail.com>
2014-12-06 19:25:30 +0800

    bugs in CPU HD IFID fixed

--- project1/Hazard_Detection_Unit.v
+++ project1/Hazard_Detection_Unit.v
@@ -4,7 +4,7 @@ module Hazard_Detection_Unit(
   input       [4:0]   IDEX_RT_addr_i,
   input               IDEX_MemRead_i,
   output reg  [31:0]  PC_o,
-  output reg          IF_ID_o,
+  output reg          IFID_o,
   output reg          flush_o
 );
 
