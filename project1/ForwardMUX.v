5f569fac75bfcd03011aac121bf6667c301640bb
Sophia <sophia6913@SophiatekiMacBook-Air.local>
2014-12-06 14:22:07 +0800

    bugs fixed ForwardMUX

--- project1/ForwardMUX.v
+++ project1/ForwardMUX.v
@@ -3,7 +3,7 @@ module ForwardMUX(
   input      [31:0] data0_i,
   input      [31:0] data1_i,
   input      [31:0] data2_i,
-  input             select_i,
+  input      [1:0]  select_i,
   output reg [31:0] data_o
 );
 
--- project1/Forward_Unit.v
+++ project1/Forward_Unit.v
@@ -1,13 +1,13 @@
 //TODO
 module Forward_Unit(
-  input        EXMEM_WB_i,//RegWrite FROM WB[1]
-  input        MEMWB_WB_i,////RegWrite FROM WB[1]
-  input  [5:0] IDEX_RsAddr_i,
-  input  [5:0] IDEX_RtAddr_i,
-  input  [5:0] EXMEM_WriteAddr_i,
-  input  [5:0] MEMWB_WriteAddr_i,
-  output reg   mux6_o,
-  output reg   mux7_o
+  input  	        EXMEM_WB_i,//RegWrite FROM WB[1]
+  input  	        MEMWB_WB_i,////RegWrite FROM WB[1]
+  input  	 [5:0]  IDEX_RsAddr_i,
+  input  	 [5:0]  IDEX_RtAddr_i,
+  input  	 [5:0]  EXMEM_WriteAddr_i,
+  input  	 [5:0]  MEMWB_WriteAddr_i,
+  output reg [1:0]  mux6_o,
+  output reg [1:0]	mux7_o
 );
 
 endmodule
