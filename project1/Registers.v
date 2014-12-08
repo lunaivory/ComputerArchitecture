fa50edc3c84593fec754fad2898c696a47b28ecd
lunaivory <catlily234@gmail.com>
2014-12-08 17:48:42 +0800

    rename register.v

--- project1/Registers.v
+++ project1/Registers.v
@@ -2,26 +2,26 @@
 module Registers
 (
     input           clk_i,
-    input   [4:0]   RSaddr_i,
-    input   [4:0]   RTaddr_i,
-    input   [4:0]   RDaddr_i, 
-    input   [31:0]  RDdata_i,
+    input   [4:0]   ReadReg1_i,//RSaddr_i,
+    input   [4:0]   ReadReg2_i,//RTaddr_i,
+    input   [4:0]   WriteReg_i,//RDaddr_i, 
+    input   [31:0]  WriteData_i,//RDdata_i,
     input           RegWrite_i, 
-    output  [31:0]  RSdata_o, 
-    output  [31:0]  RTdata_o 
+    output  [31:0]  ReadData1_o,//RSdata_o, 
+    output  [31:0]  ReadData2_o//RTdata_o 
 );
 
 // Register File
 reg     [31:0]      register        [0:31];
 
 // Read Data      
-assign  RSdata_o = register[RSaddr_i];
-assign  RTdata_o = register[RTaddr_i];
+assign  ReadData1_o = register[ReadReg1_i];
+assign  ReadData2_o = register[ReadReg2_i];
 
 // Write Data   
 always@(posedge clk_i) begin
     if(RegWrite_i)
-        register[RDaddr_i] <= RDdata_i;
+        register[WriteReg_i] <= WriteData_i;
 end
    
 endmodule 
