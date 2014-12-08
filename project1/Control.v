066f0980f0ae80272dd070a98ce2a1f615dd3bd4
lunaivory <catlily234@gmail.com>
2014-12-08 17:19:21 +0800

    酷耶～

--- project1/Control.v
+++ project1/Control.v
@@ -15,88 +15,88 @@ reg [2:0] ALUOp;
 
 always @(Op_i)begin
     if(Op_i == 6'b000000) begin //Rtype
-        ALUSrc    <= 1'b0;
-        ALUOp     <= 2'b10;
-        RegDst    <= 1'b1;
+        ALUSrc    = 1'b0;
+        ALUOp     = 2'b10;
+        RegDst    = 1'b1;
 
-		MemWrite  <= 1'b0;
-		MemRead   <= 1'b0;
+		MemWrite  = 1'b0;
+		MemRead   = 1'b0;
 
-        RegWrite  <= 1'b1;
-        MemtoReg  <= 1'b0;
+        RegWrite  = 1'b1;
+        MemtoReg  = 1'b0;
 
-        jumpCtrl_o <= 1'b0;
-        brenchCtrl_o <= 1'b0;
+        jumpCtrl_o = 1'b0;
+        brenchCtrl_o = 1'b0;
     end
     else if(Op_i == 6'b001000) begin //I type
-        ALUSrc    <= 1'b1;
-        ALUOp     <= 2'b00; // addi only
-        RegDst    <= 1'b0;
+        ALUSrc    = 1'b1;
+        ALUOp     = 2'b00; // addi only
+        RegDst    = 1'b0;
 
-		MemWrite  <= 1'b0;
-		MemRead   <= 1'b0;
+		MemWrite  = 1'b0;
+		MemRead   = 1'b0;
 
-        RegWrite  <= 1'b1;
-        MemtoReg  <= 1'b0;
+        RegWrite  = 1'b1;
+        MemtoReg  = 1'b0;
 
-        jumpCtrl_o <= 1'b0;
-        brenchCtrl_o <= 1'b0;
+        jumpCtrl_o = 1'b0;
+        brenchCtrl_o = 1'b0;
     end
     else if(Op_i == 6'b100011) begin //lw
-        ALUSrc    <= 1'b1;
-        ALUOp     <= 2'b00;
-        RegDst    <= 1'b0;
+        ALUSrc    = 1'b1;
+        ALUOp     = 2'b00;
+        RegDst    = 1'b0;
 
-		MemWrite  <= 1'b0;
-		MemRead   <= 1'b1;
+		MemWrite  = 1'b0;
+		MemRead   = 1'b1;
 
-        RegWrite  <= 1'b1;
-        MemtoReg  <= 1'b1;
+        RegWrite  = 1'b1;
+        MemtoReg  = 1'b1;
 
-        jumpCtrl_o <= 1'b0;
-        brenchCtrl_o <= 1'b0;
+        jumpCtrl_o = 1'b0;
+        brenchCtrl_o = 1'b0;
     end
     else if(Op_i == 6'b101011) begin //sw
-        ALUSrc    <= 1'b1;
-        ALUOp     <= 2'b00;
-        RegDst    <= 1'b0;
+        ALUSrc    = 1'b1;
+        ALUOp     = 2'b00;
+        RegDst    = 1'b0;
 
-		MemWrite  <= 1'b1;
-		MemRead   <= 1'b0;
+		MemWrite  = 1'b1;
+		MemRead   = 1'b0;
 
-        RegWrite  <= 1'b0;
-        MemtoReg  <= 1'b0;
+        RegWrite  = 1'b0;
+        MemtoReg  = 1'b0;
 
-        jumpCtrl_o <= 1'b0;
-        brenchCtrl_o <= 1'b0;
+        jumpCtrl_o = 1'b0;
+        brenchCtrl_o = 1'b0;
     end
     else if(Op_i == 6'b000100) begin //beq
-        ALUSrc    <= 1'b0; //don't care?
-        ALUOp     <= 2'b01;//don't care?
-        RegDst    <= 1'b1; //don't care?
+        ALUSrc    = 1'b0; //don't care?
+        ALUOp     = 2'b01;//don't care?
+        RegDst    = 1'b1; //don't care?
 
-		MemWrite  <= 1'b0;
-		MemRead   <= 1'b0;
+		MemWrite  = 1'b0;
+		MemRead   = 1'b0;
 
-        RegWrite  <= 1'b0;
-        MemtoReg  <= 1'b0;
+        RegWrite  = 1'b0;
+        MemtoReg  = 1'b0;
 
-        jumpCtrl_o <= 1'b0;
-        brenchCtrl_o <= 1'b1;
+        jumpCtrl_o = 1'b0;
+        brenchCtrl_o = 1'b1;
     end
     else if(Op_i == 6'b000010) begin //j
-        ALUSrc    <= 1'b0;
-        ALUOp     <= 2'b01;
-        RegDst    <= 1'b1;
+        ALUSrc    = 1'b0;
+        ALUOp     = 2'b01;
+        RegDst    = 1'b1;
 
-		MemWrite  <= 1'b0;
-		MemRead   <= 1'b0;
+		MemWrite  = 1'b0;
+		MemRead   = 1'b0;
 
-        RegWrite  <= 1'b0;
-        MemtoReg  <= 1'b0;
+        RegWrite  = 1'b0;
+        MemtoReg  = 1'b0;
 
-        jumpCtrl_o <= 1'b1;
-        brenchCtrl_o <= 1'b0;
+        jumpCtrl_o = 1'b1;
+        brenchCtrl_o = 1'b0;
     end
 
 	EX_o  = (ALUSrc << 3) + (ALUOp << 1) + RegDst;
