1acc6935dd6b7a327a6678a117b7f003348b8775
lunaivory <catlily234@gmail.com>
2014-12-17 22:54:36 +0800

    tested

--- project1/Hazard_Detection_Unit.v
+++ project1/Hazard_Detection_Unit.v
@@ -12,24 +12,24 @@ module Hazard_Detection_Unit(
 initial begin
 	PC_stall_o <= 0;
 end
-
+//combinational 裡面要用blocking
 always @(*) begin
-  PC_stall_o <= 1'b0;
-  IFID_stall_o <= 1'b0;
-  IDEX_stall_o <= 1'b1;
+  PC_stall_o = 1'b0;
+  IFID_stall_o = 1'b0;
+  IDEX_stall_o = 1'b1;
   if (IDEX_MemRead_i) begin
   //addi
     if ((Op_i == 6'b001000) && (IFID_RsAddr_i == IDEX_RtAddr_i)) begin 
-      PC_stall_o <= 1'b1;
-      IFID_stall_o <= 1'b1;
-      IDEX_stall_o <= 1'b0;
+      PC_stall_o = 1'b1;
+      IFID_stall_o = 1'b1;
+      IDEX_stall_o = 1'b0;
     end
   //beq or Rtype
     else if (((Op_i == 6'b000100) || (Op_i == 6'b000000)) &&
             ((IFID_RsAddr_i == IDEX_RtAddr_i) || (IFID_RtAddr_i == IDEX_RtAddr_i))) begin
-      PC_stall_o <= 1'b1;
-      IFID_stall_o <= 1'b1;
-      IDEX_stall_o <= 1'b0;
+      PC_stall_o = 1'b1;
+      IFID_stall_o = 1'b1;
+      IDEX_stall_o = 1'b0;
     end
   end
 end
